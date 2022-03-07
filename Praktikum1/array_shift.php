c95dc648f23c92682eda5bc5b1e148f4d24b45b5
dhodi yoga sahida <92575618+Dhodi4646@users.noreply.github.com>
2022-03-07 19:03:46 +0700

    Add files via upload

---
+++ Praktikum1/array_shift.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<?php
+    $tims = ["erwin", "heru", "ali", "zaki"];
+    array_shift($tims);
+    foreach($tims as $person){
+        echo $person.'<br/>';
+    }   
+?>
+</body>
+</html>
